d465df9baf0c7710f9dc94a056caa78ad0625de6
Eli Hooten <elir.hooten@gmail.com>
2021-06-03 10:22:23 -0500

    Code cleanup

--- src/Codecov/SpanConverter.php
+++ src/Codecov/SpanConverter.php
@@ -20,40 +20,6 @@ class SpanConverter
         $this->serviceName = $serviceName;
     }
 
-    /**
-     * {.
-     * "name": "codecov.views.redirect_app",
-     * "context": {
-     * "trace_id": "0xe3a8230d9f94655a04bfa686796a3fe7",
-     * "span_id": "0xff945291223e2717",
-     * "trace_state": "[]"
-     * },
-     * "kind": "SpanKind.SERVER",
-     * "parent_id": null,
-     * "start_time": "2021-05-14T14:13:40.897911Z",
-     * "end_time": "2021-05-14T14:13:57.589268Z",
-     * "status": {
-     * "status_code": "UNSET"
-     * },
-     * "attributes": {
-     * "http.method": "GET",
-     * "http.server_name": "testserver",
-     * "http.scheme": "http",
-     * "net.host.port": 80,
-     * "http.url": "http://testserver/redirect_app/gh/codecov/codecov.io/settings",
-     * "net.peer.ip": "127.0.0.1",
-     * "http.flavor": "1.1"
-     * },
-     * "events": [],
-     * "links": [],
-     * "resource": {
-     * "telemetry.sdk.language": "python",
-     * "telemetry.sdk.name": "opentelemetry",
-     * "telemetry.sdk.version": "1.2.0",
-     * "service.name": "unknown_service"
-     * }
-     * }.
-     */
     public function convert(Span $span)
     {
         $spanParent = $span->getParent();
@@ -108,18 +74,10 @@ class SpanConverter
             return $value ? 'true' : 'false';
         }
 
-        // Zipkin tags must be strings, but opentelemetry
-        // accepts strings, booleans, numbers, and lists of each.
         if (is_array($value)) {
             return join(',', array_map([$this, 'sanitizeTagValue'], $value));
         }
 
-        // Floats will lose precision if their string representation
-        // is >=14 or >=17 digits, depending on PHP settings.
-        // Can also throw E_RECOVERABLE_ERROR if $value is an object
-        // without a __toString() method.
-        // This is possible because OpenTelemetry\Trace\Span does not verify
-        // setAttribute() $value input.
         return (string) $value;
     }
 
